80e2bfe17a9d6504cbeadd2fead8b07cf8ede547
mohammad Rahmani <mrahmani788@gmail.com>
2022-06-24 13:37:22 +0200

    add projet laravel 1

--- 01-Tuto/php/laravel-users/routes/web.php
+++ 01-Tuto/php/laravel-users/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProduitController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +19,7 @@ Route::get('/', function () {
 });
 
 Route::get('/contact', function () {
-    return view('cotnact');
+    return view('contact');
 });
 
+Route::resource('produits', ProduitController::class);
