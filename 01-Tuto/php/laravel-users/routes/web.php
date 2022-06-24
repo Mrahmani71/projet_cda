9ccfa7e47e79db8ef1c82b677512ecfe020e2b6b
mohammad Rahmani <mrahmani788@gmail.com>
2022-06-24 16:51:27 +0200

    add projet laravel 1

--- 01-Tuto/php/laravel-users/routes/web.php
+++ 01-Tuto/php/laravel-users/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProduitController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
 
-Route::resource('produits', ProduitController::class);
+require __DIR__.'/auth.php';
